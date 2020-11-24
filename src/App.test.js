c7d616e4907a3ba95e8bd5741f3a004039b48a88
cmtp <christian.tola@hotmail.com>
2020-11-24 10:11:25 -0400

    add test for Todo component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,45 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { shallow, configure, EnzymeAdapter } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App, { Todo } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+configure({ adapter: new Adapter() });
+
+describe("App", () => {
+  describe('Todo', () => {
+    it('Ejecutar completeTodo cuando hago click en Complete', () => {
+      const completeTodo = jest.fn();
+      // completeTodo.mock.calls === [[1], [2, 3]. [5], [], [1, 2, 3, 5]]
+      const removeTodo = jest.fn();
+      const index = 5;
+      const todo = {
+        isCompleted: true,
+        text: 'lala',
+      }
+
+      const wrapper = shallow(<Todo completeTodo={completeTodo} removeTodo={removeTodo} index={index} todo={todo} />)
+      wrapper.find('button').at(0).simulate('click');
+      
+      expect(completeTodo.mock.calls).toEqual([[5]]);
+      expect(removeTodo.mock.calls).toEqual([]);
+    });
+
+    it('Ejecutar removeTodo cuando hago click en Remove', () => {
+      const completeTodo = jest.fn();
+      // completeTodo.mock.calls === [[1], [2, 3]. [5], [], [1, 2, 3, 5]]
+      const removeTodo = jest.fn();
+      const index = 5;
+      const todo = {
+        isCompleted: true,
+        text: 'lala',
+      }
+
+      const wrapper = shallow(<Todo completeTodo={completeTodo} removeTodo={removeTodo} index={index} todo={todo} />)
+      wrapper.find('button').at(1).simulate('click');
+      
+      expect(removeTodo.mock.calls).toEqual([[5]]);
+      expect(completeTodo.mock.calls).toEqual([]);
+
+    });
+  })
+})
